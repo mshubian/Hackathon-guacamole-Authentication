421f2fc1c20279065598203f1b40eaed490885e4
mshubian <v-bih@microsoft.com>
2015-02-03 16:37:43 +0800

    Automaticlly prase jsonString to GuacamoleConfiguration withn a while loop

--- src/main/java/com/openhackathon/guacamole/Trans2GuacdConfiguration.java
+++ src/main/java/com/openhackathon/guacamole/Trans2GuacdConfiguration.java
@@ -1,6 +1,8 @@
 package com.openhackathon.guacamole;
 
 
+import java.util.Iterator;
+
 import org.glyptodon.guacamole.protocol.GuacamoleConfiguration;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -21,19 +23,33 @@ public class Trans2GuacdConfiguration {
 
 	public Trans2GuacdConfiguration(String jsonString) {
 			
-		configuration = new GuacamoleConfiguration();		
+		configuration = new GuacamoleConfiguration();
 		try {
 			
 			JSONObject json = new JSONObject(jsonString.replace("\\", ""));
 			configuration = new GuacamoleConfiguration();
-			
-			configuration.setProtocol(json.getString("protocol"));
-			configuration.setParameter("name", json.getString("name"));
-			configuration.setParameter("username", json.getString("username"));
-			configuration.setParameter("password", json.getString("password"));
-			configuration.setParameter("hostname", json.getString("hostname"));
-			configuration.setParameter("port", json.getString("port"));
-			
+	        Iterator<String> keys = json.keys();  
+
+/**			configuration.setProtocol(json.getString("protocol"));
+ *			configuration.setParameter("name", json.getString("name"));
+ *			configuration.setParameter("username", json.getString("username"));
+ *			configuration.setParameter("password", json.getString("password"));
+ *			configuration.setParameter("hostname", json.getString("hostname"));
+ *			configuration.setParameter("port", json.getString("port"));
+ **/
+            /*Automlly set configuration value*/
+            while(keys.hasNext()){
+                String key = keys.next();
+                if (key.equals("displayname")) {
+                    continue;
+                }
+                if (key.equals("protocol")) {
+                    configuration.setProtocol(json.getString("protocol"));
+                }else {
+                    configuration.setParameter(key, json.getString(key));
+                }
+            }
+
 		} catch (Exception e) {
 			logger.error("==================Failed when transfor jsonString to GuacamoleConfiguation  ");
 			configuration = new GuacamoleConfiguration();
